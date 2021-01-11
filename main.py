3f12f9b407bf8626a0fe8b6173e84c6cf87f8b71
Dutchban <daniel.mayer22@gmail.com>
2021-01-11 13:21:40 +0100

    Finalized the update functionality

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ with pyodbc.connect(conx_string) as conx:
     cursor.execute(query)
     data = cursor.fetchall()
 
-print(data)
-
 print("Your adress book is now ready to start.")
 
 while 1:
@@ -28,20 +26,47 @@ while 1:
             data = cursor.fetchall()
         print("There you go:\n")
         for row in data:
-            print(f"FIRST NAME: {row.FirstName} LAST NAME: {row.LastName} AGE: {row.Age}")
+            print(f"FIRST NAME: {row.FirstName} LAST NAME: {row.LastName} AGE: {row.Age}\n")
     if user_input == "search_contact":
         searched_name = input("What person do you search?\n")
-        query = "SELECT FirstName FROM TestDB.dbo.Person WHERE FirstName = '%Martin%'"
+        query = "SELECT FirstName, LastName FROM TestDB.dbo.Person WHERE FirstName LIKE ?"
         with pyodbc.connect(conx_string) as conx:
             cursor = conx.cursor()
-            cursor.execute(query)
+            cursor.execute(query, searched_name)
             data = cursor.fetchall()
-        print("There you go:\n")
+        print("There you go:")
         for row in data:
-            print(f"FIRST NAME: {row.FirstName}")
-    else:
-        print("hello")
+            print(f"FIRST NAME: {row.FirstName} {row.LastName}\n")
+    if user_input == "update_contact":
+        searched_name = input("What person do you want to update?\n")
+        query = "SELECT FirstName, LastName FROM TestDB.dbo.Person WHERE FirstName LIKE ?"
+        with pyodbc.connect(conx_string) as conx:
+            cursor = conx.cursor()
+            cursor.execute(query, searched_name)
+            data = cursor.fetchall()
+        print("Okay, you update the following contact")
+        for row in data:
+            print(f"FIRST NAME: {row.FirstName} {row.LastName}\n")
+        updated_info = input("What do you want to update? Enter: FirstName, LastName or Age\n").lower()
+        if updated_info == "lastname":
+            new_info = input("Okay, what should the new LastName be?\n")
+            with pyodbc.connect(conx_string) as conx:
+                cursor = conx.cursor()
+                cursor.execute("UPDATE TestDB.dbo.Person SET LastName = ? WHERE FirstName = ?", (new_info, searched_name))
+                conx.commit()
+            print("Okay, done")
+        if updated_info == "firstname":
+            new_info = input("Okay, what should the new FirstName be?\n")
+            with pyodbc.connect(conx_string) as conx:
+                cursor = conx.cursor()
+                cursor.execute("UPDATE TestDB.dbo.Person SET FirstName = ? WHERE FirstName = ?", (new_info, searched_name))
+                conx.commit()
+            print("Okay, done")
+        if updated_info == "age":
+            new_info = input("Okay, what should the new Age be?\n")
+            with pyodbc.connect(conx_string) as conx:
+                cursor = conx.cursor()
+                cursor.execute("UPDATE TestDB.dbo.Person SET Age = ? WHERE FirstName = ?", (new_info, searched_name))
+                conx.commit()
+            print("Okay, done")
 
-
-
-    break
